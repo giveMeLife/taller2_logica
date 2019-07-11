0b16062631eaf4b79ad1a850534e32b549b5de1a
giveMeLife <j.fernandez.munoz@hotmail.com>
2019-07-10 20:20:00 -0400

    final

--- taller2.py
+++ taller2.py
@@ -41,15 +41,15 @@ tipo = sys.argv[1].lower()
 
 
 
-
 #Antecedentes
 tamano_taza['pequeno'] = fuzz.trimf(tamano_taza.universe, [0,0,150])
 tamano_taza['mediano'] = fuzz.trimf(tamano_taza.universe, [90,150,250])
-tamano_taza['grande'] = fuzz.trimf(tamano_taza.universe, [150,450, 450])
+tamano_taza['grande'] = fuzz.trimf(tamano_taza.universe, [150,420, 420])
+
 
 temperatura_ambiental['frio'] = fuzz.trimf(temperatura_ambiental.universe, [0,0,21])
 temperatura_ambiental['calido'] = fuzz.trimf(temperatura_ambiental.universe, [15,24,30])
-temperatura_ambiental['caluroso'] = fuzz.trimf(temperatura_ambiental.universe, [27,45, 45])
+temperatura_ambiental['caluroso'] = fuzz.trimf(temperatura_ambiental.universe, [27,42, 42])
 
 intensidad_cafe['suave'] = fuzz.trimf(intensidad_cafe.universe, [0,0,3])
 intensidad_cafe['fuerte'] = fuzz.trimf(intensidad_cafe.universe, [1,2,3])
@@ -60,7 +60,7 @@ intensidad_cafe['medio'] = fuzz.trimf(intensidad_cafe.universe, [3,5, 5])
 #Consecuentes
 nivel_agua['poca'] = fuzz.trimf(nivel_agua.universe, [0,0,150])
 nivel_agua['media'] = fuzz.trimf(nivel_agua.universe, [90,150,250])
-nivel_agua['mucha'] = fuzz.trimf(nivel_agua.universe, [150,450,450])
+nivel_agua['mucha'] = fuzz.trimf(nivel_agua.universe, [150,420,420])
 
 cantidad_cafe['poca'] = fuzz.trimf(cantidad_cafe.universe, [0,0,9])
 cantidad_cafe['media'] = fuzz.trimf(cantidad_cafe.universe, [7.5,9,15])
@@ -76,8 +76,7 @@ cantidad_chocolate['mucha'] = fuzz.trimf(cantidad_chocolate.universe, [13.5,15,2
 
 tiempo_preparacion['poca'] = fuzz.trimf(tiempo_preparacion.universe, [0,0,1])
 tiempo_preparacion['media'] = fuzz.trimf(tiempo_preparacion.universe, [0.8,1,2])
-tiempo_preparacion['mucha'] = fuzz.trimf(tiempo_preparacion.universe, [1.8,2,3])
-
+tiempo_preparacion['mucha'] = fuzz.trimf(tiempo_preparacion.universe, [1.8,2,2.8])
 
 ############################DESCOMENTAR SI SE QUIERE VER GRAFICOS#############################
 #graficos antecedentes
